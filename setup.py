b88a0f1602112a85481989cf78477aab7c9bc6ac
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-06-08 17:05:31 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='wildcardmedia.vimeo',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description="Vimeo adapter for a WildcardVideo content type.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
